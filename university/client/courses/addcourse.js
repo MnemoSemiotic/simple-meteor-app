7e52ec9a0f2c6a8532dc3ebfb4c00dc6f1991466
Andrew Roberts <clownfragment@gmail.com>
2017-11-06 23:16:11 -0700

    created js file for addcourse

---
+++ university/client/courses/addcourse.js
@@ -0,0 +1,16 @@
+Template.addCourse.events({
+  'click #submit': function(event, instance) {
+    if (event && event.preventDefault) event.preventDefault();
+
+    var courseInput = {};
+
+    // taking form data
+    courseInput.coursename = $("#coursename").val();
+    courseInput.coursedescription = $("#coursedescription").val();
+    courseInput.credithours = $("#credithours").val();
+
+    Meteor.call("addCourse", courseInput);
+
+    Router.go('/courses');
+  }
+})
